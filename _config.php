9fb161be48fcd3e7eabb7b10c4c05f18da8157a4
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-07-17 10:23:51 +0000

    turning off a few things in _config.php for repeat orders
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_repeatorders/trunk@1355 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -7,7 +7,7 @@
 //Object::add_extension('Order', 'AutomaticallyCreatedOrderDecorator');
 //RepeatOrder::set_delivery_days(array('Tuesday','Wednesday','Thursday','Friday'));
 //RepeatOrder::set_payment_methods(array('DirectCreditPayment'  => 'Direct Credit (payment into bank account)', 'DpsPxPayStoredPayment'  => 'Saved Credit Card using DPS'));
-	//RepeatOrder::set_minimum_days_in_the_future(3);
+//RepeatOrder::set_minimum_days_in_the_future(3);
 //===================---------------- END ecommerce_Repeatorders MODULE ----------------===================
 
 
