afc485eba80f10927dcef4659cde26120ae7f89b
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-05-01 10:00:12 +0000

    MINOR
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_repeatorders/trunk@2574 950b781c-c544-efab-2f99-90083ee69683

--- code/StandingOrdersPage.php
+++ code/StandingOrdersPage.php
@@ -253,6 +253,8 @@ class RepeatOrdersPage_Controller extends AccountPage_Controller {
 			"AllRepeatOrders" => DataObject::get("RepeatOrder", "\"Status\" = 'Active'")
 		);
 		Requirements::javascript(THIRDPARTY_DIR."/jquery/jquery.js");
+		//Requirements::block(THIRDPARTY_DIR."/jquery/jquery.js");
+		//Requirements::javascript(Director::protocol()."ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js");
 		Requirements::javascript("ecommerce_Repeatorders/javascript/RepeatOrdersPage_admin.js");
 		Requirements::themedCSS("RepeatOrdersPage_admin");
 		return $this->renderWith(array('RepeatOrdersPage_admin', 'Page'), $params);
